9df530230851e70824cd82e650f831b1768cf4ef
yas2 <yashprwal@gmail.com>
2021-04-19 16:52:00 +0530

    my first commit

---
+++ core-java-project/src/com/sapient/trg/pl/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.sapient.trg.pl;
+
+import java.util.Date;
+
+/**
+ * 
+ * @author yas2
+ * this is doc style comment
+ *
+ */
+public class HelloWorld {
+	/**
+	 * 
+	 * @param args
+	 * this is main method
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("Today is: "+new Date());
+	}
+
+}
